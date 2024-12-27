733b679e2dd8f97fea1ace0d44ae7fbc707f0c28
ivanosky144 <ivanoskytan@gmail.com>
2024-12-27 12:10:46 +0700

    feat: add binary tree data structure and traversal operations

--- src/binary_tree/main.go
+++ src/binary_tree/main.go
@@ -1,1 +1,15 @@
 package binarytree
+
+type TreeNode struct {
+	Value int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func NewNode(value int) *TreeNode {
+	return &TreeNode{
+		Value: value,
+		Left:  nil,
+		Right: nil,
+	}
+}
--- src/binary_tree/operation.go
+++ src/binary_tree/operation.go
@@ -1,1 +1,31 @@
 package binarytree
+
+func (root *TreeNode) PreOrderTraversal(result *[]int) {
+	if root == nil {
+		return
+	}
+
+	*result = append(*result, root.Value)
+	root.Left.PreOrderTraversal(result)
+	root.Right.PreOrderTraversal(result)
+}
+
+func (root *TreeNode) InOrderTraversal(result *[]int) {
+	if root == nil {
+		return
+	}
+
+	root.Left.PreOrderTraversal(result)
+	*result = append(*result, root.Value)
+	root.Right.PreOrderTraversal(result)
+}
+
+func (root *TreeNode) PostOrderTraversal(result *[]int) {
+	if root == nil {
+		return
+	}
+
+	root.Left.PreOrderTraversal(result)
+	root.Right.PreOrderTraversal(result)
+	*result = append(*result, root.Value)
+}
