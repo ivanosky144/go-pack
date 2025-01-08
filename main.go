970ce1242bf694acdc429ecb328a9b0cdc8b0e5c
ivanosky144 <ivanoskytan@gmail.com>
2025-01-08 14:01:31 +0700

    dev: restructure folders

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	_ "github.com/ivanosky144/go-pack/internal/binary_tree"
-	_ "github.com/ivanosky144/go-pack/internal/hash_set"
-	_ "github.com/ivanosky144/go-pack/internal/linked_list"
-	_ "github.com/ivanosky144/go-pack/internal/n_ary_tree"
-	_ "github.com/ivanosky144/go-pack/internal/queue"
-	_ "github.com/ivanosky144/go-pack/internal/stack"
+	_ "github.com/ivanosky144/go-pack/binary_tree"
+	_ "github.com/ivanosky144/go-pack/hash_set"
+	_ "github.com/ivanosky144/go-pack/linked_list"
+	_ "github.com/ivanosky144/go-pack/n_ary_tree"
+	_ "github.com/ivanosky144/go-pack/queue"
+	_ "github.com/ivanosky144/go-pack/stack"
 )
 
 func main() {
